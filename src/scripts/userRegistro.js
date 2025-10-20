3d50be4d6bb2a6ec0df49b125f5551bc66c9ce01
codigoypunto <codigo.ypunto@gmail.com>
2025-10-20 09:18:34 -0500

    activacion de registro

--- src/scripts/userRegistro.js
+++ src/scripts/userRegistro.js
@@ -4,10 +4,29 @@ document.getElementById('registrationForm').addEventListener(
 
         e.preventDefault();
 
-        /* 
+        
 
         const dni           = document.getElementById("dni").value;
         const company       = document.getElementById("company").value;
+        const lugar         = document.getElementById("lugar").value;
+
+        const googleFormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScOEQGcKyg6R1vZQs1BFm3Rt4bi1GNG-dfWdu1PdmjEs_ZqxQ/formResponse";
+
+        const formData = new FormData();
+
+        formData.append("entry.1245377593", dni);
+        formData.append("entry.1112581709", company);
+        formData.append("entry.1612326977", lugar)
+
+        fetch(
+            googleFormUrl, {
+                method: "POST",
+                mode: "no-cors",
+                body: formData
+            }
+        )
+
+        /*
 
         // const googleFormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScZnJW2-pnDDRpOOlnNxM6D4yaEUFbCZRWKtMDSvjrXVsUkHw/formResponse";
         const googleFormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeRfJ0mYkdGaAQ4qw7GCP8e0ZlIoiJYmtesrygTYcAJomjnvg/formResponse";
